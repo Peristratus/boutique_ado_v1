cebcc6bfc6a8a843d4c743d5350588940756b783
Peristratus <testcorp24@gmail.com>
2021-03-20 16:39:34 +0000

    customized admin

--- products/models.py
+++ products/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
+
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -9,6 +14,7 @@ class Category(models.Model):
 
     def get_friendly_name(self):
         return self.friendly_name
+
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True,
@@ -25,4 +31,3 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-

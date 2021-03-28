1e77d57a36787785da695eb58f57a7338bdd78c4
Peristratus <testcorp24@gmail.com>
2021-03-28 22:10:31 +0000

    added the stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Ia4WcB87QiGthiQ6RsSj9JW0J7I6Yd8I7DrvlUXLDZiq9axTSW7IAjMtJUw1bodA3Njbc9Zpd89RXGhQc0OgQye00wLe6Ll0E',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

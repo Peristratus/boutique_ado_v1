74b3e9506a33bb57e0851f6883c366cd256609f3
Peristratus <testcorp24@gmail.com>
2021-03-29 03:06:45 +0000

    added the stripee stripe API but getting an errorneed to sort out the bug

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -6,9 +6,9 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripe_public_key =$('#id_stripe_public_key').text().slice(1, -1);
-var client_secret =$('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var card = elements.create('card', {style: style});
 var style = {
@@ -26,4 +26,23 @@ var style = {
         iconColor: '#dc3545'
     }
 };
+
 card.mount('#card-element');
+
+//  Handel realtime validation error on the card element
+card.addEventListner('change', function (event){
+    var errorDiv =document.getElementById('card-errors');
+    if (event.error){
+        var html= `
+            <span  class="icon" role="alert">
+                <i class ="fas fa-times"></i>
+            </span>
+             <span>${event.error.message}</span>
+         `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent ='';
+
+    }
+});
+

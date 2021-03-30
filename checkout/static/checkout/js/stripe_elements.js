0c4b72aec7a40ec193a66ec9739d28b0b006721d
Peristratus <testcorp24@gmail.com>
2021-03-30 23:52:12 +0000

    added the spinnner and stripe pop code need to fix the bug

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -2,7 +2,6 @@
     Core logic/payment flow for this comes from here:
     https://stripe.com/docs/payments/accept-a-payment
     CSS from here: 
-
     https://stripe.com/docs/stripe-js
 */
 
@@ -10,7 +9,6 @@ var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
-var card = elements.create('card', {style: style});
 var style = {
     base: {
         color: '#000',
@@ -26,25 +24,25 @@ var style = {
         iconColor: '#dc3545'
     }
 };
-
+var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
-//  Handel realtime validation error on the card element
-card.addEventListner('change', function (event){
-    var errorDiv =document.getElementById('card-errors');
-    if (event.error){
-        var html= `
-            <span  class="icon" role="alert">
-                <i class ="fas fa-times"></i>
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
             </span>
-             <span>${event.error.message}</span>
-         `;
+            <span>${event.error.message}</span>
+        `;
         $(errorDiv).html(html);
     } else {
-        errorDiv.textContent ='';
-
+        errorDiv.textContent = '';
     }
 });
+
 // Handle form submit
 var form = document.getElementById('payment-form');
 
@@ -52,6 +50,8 @@ form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -65,6 +65,8 @@ form.addEventListener('submit', function(ev) {
                 </span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
             card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
         } else {

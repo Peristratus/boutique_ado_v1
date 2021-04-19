8b8f15088e2679a2e5f842ff868bff5d47b99c26
Peristratus <testcorp24@gmail.com>
2021-04-19 01:08:59 +0000

    cannot checkout as a non registered user get an error message and webhooks test failed

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+
 from .models import UserProfile
 from .forms import UserProfileForm
 
 from checkout.models import Order
 
-
-@login_required
 def profile(request):
-    """ Dispaly the users Profile. """
+    """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
@@ -17,10 +15,8 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-        else:
-            messages.error(request, 'Update failed Please ensure the form is valid')
-    else:
-         form = UserProfileForm(instance=profile)
+
+    form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
@@ -32,9 +28,9 @@ def profile(request):
 
     return render(request, template, context)
 
+
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
-
 
     messages.info(request, (
         f'This is a past confirmation for order number {order_number}. '
@@ -43,9 +39,8 @@ def order_history(request, order_number):
 
     template = 'checkout/checkout_success.html'
     context = {
-        'order':order,
+        'order': order,
         'from_profile': True,
     }
 
     return render(request, template, context)
-

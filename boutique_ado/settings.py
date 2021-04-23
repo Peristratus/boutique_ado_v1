6fbd80e86349000f50132ef66ab920bf41bf1b9c
Peristratus <testcorp24@gmail.com>
2021-04-23 20:45:19 +0000

    adjusted line 198 and 201 again

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -199,9 +199,9 @@ if 'USE_AWS' in os.environ:
 
     # Static and media file
     STATICFILES_STORAGE ='storages.backends.s3boto3.S3Boto3Storage'
-    STATICFILES_LOCATION ='/static/'
+    STATICFILES_LOCATION ='static/'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-    MEDIAFILES_LOCATION = '/media/'
+    MEDIAFILES_LOCATION = 'media/'
 
     # Overide static and media URLs in Production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'

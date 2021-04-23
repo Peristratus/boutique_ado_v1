1ab3f410b90a6eaf0515d32be2bb017a303cc1fd
Peristratus <testcorp24@gmail.com>
2021-04-23 17:12:12 +0000

    corrected typo error on line 190 changed BUDGET to BUCKET

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -187,7 +187,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'ca-central-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUDGET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media file
     STATICFILES_STORAGE ='custom_storages.StaticStorage'

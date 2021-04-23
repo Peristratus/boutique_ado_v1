029730a84f5ab6d15e72bbee557566fe2f953618
Peristratus <testcorp24@gmail.com>
2021-04-23 18:50:22 +0000

    Added cache control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -181,7 +181,15 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 if 'USE_AWS' in os.environ:
+    # Cache Control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+
     #Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado-cbc'
     AWS_S3_REGION_NAME = 'ca-central-1'

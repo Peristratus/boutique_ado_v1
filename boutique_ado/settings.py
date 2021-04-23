c71c2d4248e4bfbb06e41193660c6da98ecdd47c
Peristratus <testcorp24@gmail.com>
2021-04-23 20:33:59 +0000

    adjusted line 198 and 201

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -195,10 +195,10 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'ca-central-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media file
-    STATICFILES_STORAGE ='storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE ='custom_storages.StaticStorage'
     STATICFILES_LOCATION ='static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'

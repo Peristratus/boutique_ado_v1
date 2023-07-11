83ec5d81e4a47f19b923fd30cf4b993c63d0f61d
Peristratus <testcorp24@gmail.com>
2023-07-11 23:57:59 +0000

    made change to Allowed_Hots

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['boutique-ado-cbc.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-peristratus-boutiqueado-ijb0t1gk3wm.ws-us101.gitpod.io', 'localhost']
 
 
 # Application definition
